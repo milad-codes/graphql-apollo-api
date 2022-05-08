44c7f530ac8d760fc5960358a5e335bb23cb7b3a
Milad Akbari <miladakbari.dev@gmail.com>
2022-05-08 10:46:47 +0430

    Crud Operation API with graphql

--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,41 @@
+const Post = require('./models/Post.model')
+
 const resolvers = {
-    Query: {
-        hello: () => {
-            return "Hello world"
+  Query: {
+    getAllPosts: async () => {
+      return await Post.find()
+    },
+    getPost: async (parent, { id }, context, info) => {
+      return await Post.findById(id)
+    },
+  },
+
+  Mutation: {
+    createPost: async (parent, args, context, info) => {
+      const { title, description } = args.post
+      return await new Post({ title, description }).save()
+    },
+
+    deletePost: async (parent, args, context, info) => {
+      await Post.findByIdAndDelete(args.id)
+      return 'Post deleted successfully!'
+    },
+
+    updatePost: async (parent, args, context, info) => {
+      const { title, description } = args.post
+
+      return await Post.findByIdAndUpdate(
+        args.id,
+        {
+          title,
+          description,
+        },
+        {
+          new: true,
         }
-    }
+      )
+    },
+  },
 }
 
 module.exports = resolvers
